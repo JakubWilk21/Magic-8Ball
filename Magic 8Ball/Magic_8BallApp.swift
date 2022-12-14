dd575bff0b25e4d2cb24d1ce9ca5a67f884e9b46
Jakub <Wilk>
2022-12-14 09:28:09 -0600

    Initial Commit

---
+++ Magic 8Ball/Magic_8BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_8BallApp.swift
+//  Magic 8Ball
+//
+//  Created by Jakub Wilk on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Magic_8BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
