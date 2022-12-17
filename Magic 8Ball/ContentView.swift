3c1694c23bf07181a7d13d7f47aebbb0930593f7
Jakub <Wilk>
2022-12-16 18:18:07 -0600

    merged

--- Magic 8Ball/ContentView.swift
+++ Magic 8Ball/ContentView.swift
@@ -8,25 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var text = "Shake Me"
+    @State var answer = "Shake Me"
     let textArray = ["Reply hazy, try again.","Ask again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","It is Certain.","It is decidedly so.","Without a doubt.","Yes definitely.","You may rely on it.","As I see it, yes.","Most likely.","Outlook good.","Yes.","Signs point to yes.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
     var body: some View {
         NavigationView{
             ZStack{
                 
+//https://developer.apple.com/documentation/swiftui/view/scaleeffect(_:anchor:)-7q7as website i used to find out how to make put my text on an image
+                
                 Image("8ball 0")
                     .scaleEffect(CGSize(width: 0.1, height: 0.1))
-                Text(text)
+                Text(answer)
                     .foregroundColor(.white)
+                    .bold()
+                    .font(.title)
                     .frame(width: 140, height: 140)
                     .multilineTextAlignment(.center)
                     .onTapGesture {
-                        text = textArray[Int.random(in: 0..<textArray.count)]
+                answer=textArray[Int.random(in:0..<textArray.count)]
                         
-                        struct ContentView_Previews: PreviewProvider {
-                            static var previews: some View {
-                                ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
                             }
                         }
-                        
-                    }}}}}
+                    }}}
+    }}
